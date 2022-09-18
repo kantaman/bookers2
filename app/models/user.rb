31b4e7bfe2dffda993b98b909fb2fe4a442a4597
EC2 Default User <ec2-user@ip-172-31-0-151.ec2.internal>
2022-09-18 08:29:40 +0000

    [Finish] アクセス制限の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :books, dependent: :destroy
   has_one_attached :profile_image
-  
-  def get_profile_image(width, height)
+
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
+
+  def get_profile_image(size)
     unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/png')
     end
-    profile_image.variant(resize_to_limit: [width, height]).processed
+    profile_image.variant(resize: size).processed
   end
 end
